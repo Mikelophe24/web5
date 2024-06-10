979cb821c98a8a7e82ba2716c739b7bb2a04590e
Mikelophe24 <153093580+Mikelophe24@users.noreply.github.com>
2024-06-10 22:20:00 +0700

    Add files via upload

---
+++ khoa.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2024 at 03:03 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qlkhoa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khoa`
+--
+
+CREATE TABLE `khoa` (
+  `id` varchar(50) NOT NULL,
+  `makhoa` varchar(50) NOT NULL,
+  `tenkhoa` varchar(50) NOT NULL,
+  `scbgd` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `khoa`
+--
+
+INSERT INTO `khoa` (`id`, `makhoa`, `tenkhoa`, `scbgd`) VALUES
+('002', 'DMCS ', 'Hóa', '3 ');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
